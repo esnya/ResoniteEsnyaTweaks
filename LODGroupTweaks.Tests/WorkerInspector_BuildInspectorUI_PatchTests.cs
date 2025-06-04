a31c4f1a778e4b650dca407b42ee3e2ddb35076f
esnya <2088693+esnya@users.noreply.github.com>
2025-06-04 19:05:18 +0900

    test: 🧪 add minimal tests for all projects

---
+++ LODGroupTweaks.Tests/WorkerInspector_BuildInspectorUI_PatchTests.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+using FluentAssertions;
+using FrooxEngine;
+using HarmonyLib;
+using Xunit;
+
+namespace EsnyaTweaks.LODGroupTweaks.Tests;
+
+public static class WorkerInspector_BuildInspectorUI_PatchTests
+{
+    [Fact]
+    public static void Patch_Should_Have_HarmonyPatch_Attribute()
+    {
+        var patchType = typeof(LODGroupTweaksMod).Assembly.GetType(
+            "EsnyaTweaks.LODGroupTweaks.LODGroup_WorkerInspector_BuildInspectorUI_Patch",
+            true
+        )!;
+        patchType.GetCustomAttribute<HarmonyPatch>().Should().NotBeNull();
+    }
+
+    [Fact]
+    public static void Patch_Should_Target_WorkerInspector_BuildInspectorUI()
+    {
+        var patchType = typeof(LODGroupTweaksMod).Assembly.GetType(
+            "EsnyaTweaks.LODGroupTweaks.LODGroup_WorkerInspector_BuildInspectorUI_Patch",
+            true
+        )!;
+        var attribute = patchType.GetCustomAttribute<HarmonyPatch>();
+        attribute.Should().NotBeNull();
+        attribute!.info.declaringType.Should().Be<WorkerInspector>();
+        attribute.info.methodName.Should().Be(nameof(WorkerInspector.BuildInspectorUI));
+    }
+
+    [Fact]
+    public static void Patch_Should_Have_Postfix_Method()
+    {
+        var patchType = typeof(LODGroupTweaksMod).Assembly.GetType(
+            "EsnyaTweaks.LODGroupTweaks.LODGroup_WorkerInspector_BuildInspectorUI_Patch",
+            true
+        )!;
+        var postfix = patchType.GetMethod("Postfix", BindingFlags.Static | BindingFlags.NonPublic);
+        postfix.Should().NotBeNull();
+        postfix!.IsStatic.Should().BeTrue();
+    }
+}
---
+++ PhotonDustTweaks.Tests/PhotonDustTweaksModTests.cs
@@ -0,0 +1,49 @@
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+
+namespace EsnyaTweaks.PhotonDustTweaks.Tests;
+
+public static class PhotonDustTweaksModTests
+{
+    [Fact]
+    public static void Mod_Should_Have_Valid_Name()
+    {
+        var mod = new PhotonDustTweaksMod();
+        mod.Name.Should().Be("EsnyaTweaks.PhotonDustTweaks");
+    }
+
+    [Fact]
+    public static void Mod_Should_Have_Valid_Author()
+    {
+        var mod = new PhotonDustTweaksMod();
+        mod.Author.Should().Be("esnya");
+    }
+
+    [Fact]
+    public static void Mod_Should_Have_Valid_Version()
+    {
+        var mod = new PhotonDustTweaksMod();
+        mod.Version.Should().MatchRegex(@"^\d+\.\d+\.\d+(?:\+[A-Za-z][A-Za-z0-9]*)?$");
+    }
+
+    [Fact]
+    public static void HarmonyId_Should_Be_Valid()
+    {
+        var harmonyId = typeof(PhotonDustTweaksMod)
+            .GetProperty("HarmonyId", BindingFlags.NonPublic | BindingFlags.Static)
+            ?.GetValue(null)
+            ?.ToString();
+
+        harmonyId.Should().NotBeNullOrEmpty();
+        harmonyId.Should().Contain("com.nekometer.esnya");
+        harmonyId.Should().Contain("PhotonDustTweaks");
+    }
+
+    [Fact]
+    public static void Mod_Should_Implement_ResoniteMod()
+    {
+        var mod = new PhotonDustTweaksMod();
+        mod.Should().BeAssignableTo<ResoniteModLoader.ResoniteMod>();
+    }
+}
