f2691fd20d41bcafd905ec5ca1184adb3bb78692
esnya <2088693+esnya@users.noreply.github.com>
2025-06-04 19:50:09 +0900

    test(uni-log): :white_check_mark: add patch behavior tests

---
+++ UniLogTweaks.Tests/UniLog_PatchBehaviorTests.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+
+namespace EsnyaTweaks.UniLogTweaks.Tests;
+
+public static class UniLog_PatchBehaviorTests
+{
+    private static Type GetPatchType() =>
+        typeof(UniLogTweaksMod).Assembly.GetType("EsnyaTweaks.UniLogTweaks.UniLog_Patch", true)!;
+
+    [Fact]
+    public static void Patch_Should_Add_Indent_And_Disable_StackTrace()
+    {
+        var patch = GetPatchType()
+            .GetMethod("Patch", BindingFlags.Static | BindingFlags.NonPublic)!;
+        object[] args = { "line1\nline2", true, false };
+        patch.Invoke(null, args);
+
+        args[0].Should().Be("line1\n\tline2");
+        args[1].Should().Be(false);
+    }
+
+    [Fact]
+    public static void Patch_Should_Leave_Message_When_No_Newline()
+    {
+        var patch = GetPatchType()
+            .GetMethod("Patch", BindingFlags.Static | BindingFlags.NonPublic)!;
+        const string original = "single line";
+        object[] args = { original, true, true };
+        patch.Invoke(null, args);
+
+        args[0].Should().Be(original);
+        args[1].Should().Be(true);
+    }
+}
